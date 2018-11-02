c40b4dd477bbfe7634b174c907af848e4ae258d6
Kien Mai Ngoc <kienmn97@gmail.com>
2018-11-02 16:12:59 +0700

    Change websocket address

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -4,13 +4,15 @@ import './App.css';
 import openSocket from 'socket.io-client'
 import ReactSpeedometer from 'react-d3-speedometer'
 
+const websocketServer = 'fit5.fit-uet.tk:9001'
+
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
       'Temperature': 0
     }
-    const socket = openSocket('localhost:9001')
+    const socket = openSocket(websocketServer)
     socket.on('send data', (json) => {
       console.log(json)
       this.setState({
