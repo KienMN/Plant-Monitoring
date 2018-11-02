2e8b89c5d3e8d1d2abb5793bd650bc71a7863ef5
Kien Mai Ngoc <kienmn97@gmail.com>
2018-11-02 15:31:04 +0700

    Adding mqtt client

---
+++ server/mqtt-client/client-subscribe.py
@@ -0,0 +1,37 @@
+import paho.mqtt.client as mqtt
+import time
+from socketIO_client import SocketIO
+
+broker = 'broker.hivemq.com'
+
+def on_log(client, userdata, level, buff):
+  print('Log', buff)
+
+def on_connect(client, userdata, flags, rc):
+  if rc == 0:
+    print("Connected")
+    client.subscribe('AT2018/Temperature')
+    client.subscribe('AT2018/Humidity')
+  else:
+    print("Bad connection. Returned code", rc)
+
+def on_disconnect(client, userdata, flags, rc = 0):
+  print("Disconnected with returned code", rc)
+
+def on_message(client, userdata, msg):
+  topic = msg.topic
+  message = str(msg.payload.decode('UTF-8'))
+  print(topic, message)
+  # with SocketIO('localhost', 8080) as socketio:
+  #   socketio.emit('my event', {'topic': topic, 'message': message})
+
+client = mqtt.Client(client_id='testtttt1')
+# client.on_log = on_log
+client.on_connect = on_connect
+client.on_message = on_message
+client.on_disconnect = on_disconnect
+
+print('Connecting to broker', broker)
+
+client.connect(broker)
+client.loop_forever()
---
+++ server/mqtt-client/client-test-publish.py
@@ -0,0 +1,40 @@
+import paho.mqtt.client as mqtt
+import time
+
+broker = 'broker.hivemq.com'
+
+def on_log(client, userdata, level, buff):
+  print('Log', buff)
+
+def on_connect(client, userdata, flags, rc):
+  if rc == 0:
+    print("Connected")
+  else:
+    print("Bad connection. Returned code", rc)
+
+def on_disconnect(client, userdata, flags, rc = 0):
+  print("Disconnected with returned code", rc)
+
+def on_message(client, userdata, msg):
+  topic = msg.topic
+  message = str(msg.payload)
+  print(topic, message)
+
+client = mqtt.Client(client_id='testtttt2')
+# client.on_log = on_log
+client.on_connect = on_connect
+client.on_message = on_message
+client.on_disconnect = on_disconnect
+
+print('Connecting to broker', broker)
+client.connect(broker)
+
+# client.subscribe('AT2018/Temperature')
+i = 1
+while(True):
+  client.loop_start()
+  client.publish('AT2018/Temperature', str(i).encode('UTF-8'))
+  time.sleep(20)
+  client.loop_stop()
+  i += 1
+client.disconnect()
