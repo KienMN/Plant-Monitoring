fe722af4ede02c57e8c5a0165976fe00e424085d
Kien Mai Ngoc <kienmn97@gmail.com>
2018-11-05 12:20:41 +0700

    Web app adding components dashboard and parameter display

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,63 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import openSocket from 'socket.io-client'
-import ReactSpeedometer from 'react-d3-speedometer'
-import { Button, Row, Col } from 'react-bootstrap'
-
-const websocketServer = 'localhost:9001'
+import Dashboard from './components/Dashboard'
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      'Temperature': 0,
-      'Humidity': 0,
-      'SoilMoisture': 0
-    }
-    let socket = openSocket(websocketServer)
-    socket.on('send data', (json) => {
-      console.log(json)
-      switch (json.topic) {
-        case "AT2018/Temperature":
-          this.setState({
-            'Temperature': parseInt(json.message, 10)
-          })
-          break
-        case "AT2018/Humidity":
-          this.setState({
-            'Humidity': parseInt(json.message, 10)
-          })
-          break
-        case "AT2018/SoilMoisture":
-          let soilMoisture = parseInt(json.message, 10)
-          soilMoisture = (1024 - soilMoisture) * 100 / 1024
-          this.setState({
-            'SoilMoisture': soilMoisture
-          })
-          break
-        default:
-          break
-      }
-    })
-  }
-
   render() {
     return (
       <div className="App">
-        <Row>
-          <Col md={3} sm={6} xs={12}>
-            <p>Temperature</p>
-            <ReactSpeedometer value={this.state.Temperature} minValue={-50} maxValue={50} startColor="#ff471a" endColor="#ff471a" />
-          </Col>
-          <Col md={3} sm={6} xs={12}>
-            <p>Humidity</p>
-            <ReactSpeedometer value={this.state.Humidity} minValue={0} maxValue={100} startColor="#ff471a" endColor="#ff471a" />
-          </Col>
-        </Row>
-        <p>Soil moisture</p>
-        <ReactSpeedometer value={this.state.SoilMoisture} minValue={0} maxValue={100} startColor="#ff471a" endColor="#ff471a" />
-        <Button bsStyle="primary">Click me</Button>
+        <Dashboard />
       </div>
     );
   }
